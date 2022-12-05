26635c38ea4a231e899da6aa56fdb2662ee46289
Jonathan <jonathanpetersen2016@gmail.com>
2022-12-05 19:38:28 -0300

    WIP

--- script/script.js
+++ script/script.js
@@ -1,7 +1,9 @@
-$(document).ready(function () {
-  $('.step-number').click(function () {
-    $('.step-number').removeClass('active');
-    $(this).addClass('active')
+$('.step-number').click(function () {
+  $('.step-number').removeClass('active');
+  $(this).addClass('active')
+});
 
-  });
+$('.step-number')[1].addEventListener('click', () => {
+  $('#step_1').addClass('inactive')
+  $('#step_2').removeClass('inactive')
 })
