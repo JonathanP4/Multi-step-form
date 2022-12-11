b2e4bbc020b4f77235550ede0a2f215fc8050e1a
Jonathan <jonathanpetersen2016@gmail.com>
2022-12-11 19:35:23 -0300

    WIP (still...lol)

--- script/script.js
+++ script/script.js
@@ -1,24 +1,40 @@
 'use strict';
 
-$('.step-number').click(function () {
-  $('.step-number').removeClass('active');
-  $(this).addClass('active')
-})
-$('.step-number')[0].addEventListener('click', () => {
-  $('.form').addClass('inactive')
-  $('#step_1').removeClass('inactive')
-})
-$('.step-number')[1].addEventListener('click', () => {
-  $('.form').addClass('inactive')
-  $('#step_2').removeClass('inactive')
-})
-$('.step-number')[2].addEventListener('click', () => {
-  $('.form').addClass('inactive')
-  $('#step_3').removeClass('inactive')
-})
-$('.step-number')[3].addEventListener('click', () => {
-  $('.form').addClass('inactive')
-  $('#step_4').removeClass('inactive')
-})
+const allNums = document.querySelectorAll('.step-number')
+const allForms = document.querySelectorAll('.form')
 
+const nextStepBtn = document.querySelectorAll('.next-step-btn')
+const previousStepBtn = document.querySelectorAll('.back-btn')
 
+const toggleStates = () => {
+  allForms.forEach(form => {
+    form.classList.add('inactive')
+  })
+  allNums.forEach(num => {
+    num.classList.remove('active')
+  })
+}
+
+for (let i = 0; i < allNums.length; i++) {
+  allNums[i].addEventListener('click', () => {
+    toggleStates()
+    document.getElementById(`step_${i + 1}`).classList.remove('inactive')
+    document.getElementById(`${i + 1}`).classList.add('active')
+  })
+}
+
+for (let i = 0; i < nextStepBtn.length; i++) {
+  nextStepBtn[i].addEventListener('click', () => {
+    toggleStates()
+    document.getElementById(`step_${i + 2}`).classList.remove('inactive')
+    document.getElementById(`${i + 2}`).classList.add('active')
+  })
+}
+
+for (let i = 1; i < previousStepBtn.length; i++) {
+  previousStepBtn[i].addEventListener('click', () => {
+    toggleStates()
+    document.getElementById(`step_${i}`).classList.toggle('inactive')
+    document.getElementById(`${i}`).classList.toggle('active')
+  })
+}
