f3e136fd02c9ef7065bfb6be86aa369901bb2ead
Jonathan <jonathanpetersen2016@gmail.com>
2022-12-11 13:50:11 -0300

    WIP, this gonnas take some time...

--- script/script.js
+++ script/script.js
@@ -1,9 +1,24 @@
+'use strict';
+
 $('.step-number').click(function () {
   $('.step-number').removeClass('active');
   $(this).addClass('active')
-});
-
+})
+$('.step-number')[0].addEventListener('click', () => {
+  $('.form').addClass('inactive')
+  $('#step_1').removeClass('inactive')
+})
 $('.step-number')[1].addEventListener('click', () => {
-  $('#step_1').addClass('inactive')
+  $('.form').addClass('inactive')
   $('#step_2').removeClass('inactive')
 })
+$('.step-number')[2].addEventListener('click', () => {
+  $('.form').addClass('inactive')
+  $('#step_3').removeClass('inactive')
+})
+$('.step-number')[3].addEventListener('click', () => {
+  $('.form').addClass('inactive')
+  $('#step_4').removeClass('inactive')
+})
+
+
