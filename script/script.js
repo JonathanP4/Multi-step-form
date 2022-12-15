30a5c04c0eea09acf2e66b520d44036ced39bffc
Jonathan <jonathanpetersen2016@gmail.com>
2022-12-15 09:19:34 -0300

    Still a lotta work left to do but we gettin there

--- script/script.js
+++ script/script.js
@@ -38,3 +38,59 @@ for (let i = 1; i < previousStepBtn.length; i++) {
     document.getElementById(`${i}`).classList.toggle('active')
   })
 }
+let planTime;
+let selectedPlan = $('#selected-plan');
+
+$('.plan').click(function () {
+  $('.plan').removeClass("plan-active");
+  $(this).addClass("plan-active");
+
+  let planOfChoice = $('.plan-active h1').text();
+  selectedPlan.text(`${planOfChoice}`)
+});
+const planPrices = $('.detail-price');
+const addonPrices = $('.price')
+
+$('#range_input').click(function () {
+  if ($('#range_input').val() === '0') {
+    planPrices[0].textContent = '$9/mo'
+    planPrices[1].textContent = '$12/mo'
+    planPrices[2].textContent = '$15/mo'
+
+    addonPrices[0].textContent = '$1/mo'
+    addonPrices[1].textContent = '$2/mo'
+    addonPrices[2].textContent = '$2/mo'
+
+    planTime = '(Monthly)'
+    $('#time').text(planTime)
+  } else {
+    planPrices[0].textContent = '$90/yr'
+    planPrices[1].textContent = '$120/yr'
+    planPrices[2].textContent = '$150/yr'
+
+    addonPrices[0].textContent = '$10/yr'
+    addonPrices[1].textContent = '$20/yr'
+    addonPrices[2].textContent = '$20/yr'
+
+    planTime = '(Yearly)'
+    $('#time').text(planTime)
+  }
+})
+
+/*const services = document.querySelectorAll('.services')
+services.forEach(el => {
+  el.addEventListener('click', () => {
+    el.classList.toggle('addon-active')
+  })
+})*/
+$('.services').each(function () {
+  $('.services').click(function () {
+    $(this).toggleClass('addon-active')
+  })
+})
+
+
+
+
+
+
